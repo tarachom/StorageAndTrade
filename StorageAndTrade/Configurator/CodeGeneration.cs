9fd31beb1cdefcfa3a7009dd5f312fdf79827b58
tarachom <tarachom@i.ua>
2021-09-07 13:55:05 +0300

    start

---
+++ StorageAndTrade/Configurator/CodeGeneration.cs
@@ -0,0 +1,84 @@
+﻿
+/*
+Copyright (C) 2019-2020 TARAKHOMYN YURIY IVANOVYCH
+All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+/*
+Автор:    Тарахомин Юрій Іванович
+Адреса:   Україна, м. Львів
+Сайт:     accounting.org.ua
+*/
+  
+/*
+ *
+ * Конфігурації "Зберігання та Торгівля"
+ * Автор Тарахомин Юрій Іванович, Україна, м. Львів, accounting.org.ua, tarachom@gmail.com
+ * Дата конфігурації: 07.09.2021 13:54:22
+ *
+ */
+
+using System;
+using System.Collections.Generic;
+using AccountingSoftware;
+
+namespace StorageAndTrade_1_0
+{
+    static class Config
+    {
+        public static Kernel Kernel { get; set; }
+		
+        public static void ReadAllConstants()
+        {
+            
+        }
+    }
+}
+
+namespace StorageAndTrade_1_0.Константи
+{
+    
+}
+
+namespace StorageAndTrade_1_0.Довідники
+{
+    
+}
+
+namespace StorageAndTrade_1_0.Перелічення
+{
+    
+}
+
+namespace StorageAndTrade_1_0.Документи
+{
+    
+}
+
+namespace StorageAndTrade_1_0.Журнали
+{
+
+}
+
+namespace StorageAndTrade_1_0.РегістриВідомостей
+{
+    
+}
+
+namespace StorageAndTrade_1_0.РегістриНакопичення
+{
+    
+}
+  
