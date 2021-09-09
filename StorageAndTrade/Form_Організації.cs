6abf45620f48e9bc6d4b3b407b9fa5bc150a3bbf
tarachom <tarachom@i.ua>
2021-09-09 10:32:42 +0300

    up

---
+++ StorageAndTrade/Form_Організації.cs
@@ -0,0 +1,224 @@
+﻿/*
+Copyright (C) 2019-2020 TARAKHOMYN YURIY IVANOVYCH
+All rights reserved.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+/*
+Автор:    Тарахомин Юрій Іванович
+Адреса:   Україна, м. Львів
+Сайт:     accounting.org.ua
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using AccountingSoftware;
+using Конфа = StorageAndTrade_1_0;
+using Константи = StorageAndTrade_1_0.Константи;
+using Довідники = StorageAndTrade_1_0.Довідники;
+using Перелічення = StorageAndTrade_1_0.Перелічення;
+
+namespace StorageAndTrade
+{
+    public partial class Form_Організації : Form
+    {
+        public Form_Організації()
+        {
+            InitializeComponent();
+        }
+
+		#region DirectoryControl OpenForm
+
+		/// <summary>
+		/// Ссилка на елемент довідника
+		/// </summary>
+		public DirectoryPointer DirectoryPointerItem { get; set; }
+
+		/// <summary>
+		/// Контрол який викликав вибір
+		/// </summary>
+		public DirectoryControl DirectoryControlItem { get; set; }
+
+        #endregion
+
+        private void FormCash_Load(object sender, EventArgs e)
+        {
+			dataGridViewRecords.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+
+			RecordsBindingList = new BindingList<Записи>();
+			dataGridViewRecords.DataSource = RecordsBindingList;
+
+			dataGridViewRecords.Columns.Add(new DataGridViewImageColumn() { Name = "Image", HeaderText = "", Width = 30, DisplayIndex = 0, Image = Properties.Resources.doc_text_image });
+			dataGridViewRecords.Columns["ID"].Visible = false;
+			dataGridViewRecords.Columns["Назва"].Width = 300;
+
+			LoadRecords();
+		}
+
+		private BindingList<Записи> RecordsBindingList { get; set; }
+
+		public void LoadRecords()
+		{
+			int selectRow = dataGridViewRecords.SelectedRows.Count > 0 ?
+				dataGridViewRecords.SelectedRows[dataGridViewRecords.SelectedRows.Count - 1].Index : 0;
+
+			RecordsBindingList.Clear();
+
+			Довідники.Організації_Select організації_Select = new Довідники.Організації_Select();
+			організації_Select.QuerySelect.Field.Add(Довідники.Організації_Select.Назва);
+
+			//ORDER
+			організації_Select.QuerySelect.Order.Add(Довідники.Організації_Select.Назва, SelectOrder.ASC);
+
+			організації_Select.Select();
+			while (організації_Select.MoveNext())
+			{
+				Довідники.Організації_Pointer cur = організації_Select.Current;
+
+				RecordsBindingList.Add(new Записи(
+					cur.UnigueID.ToString(),
+					cur.Fields[Довідники.Організації_Select.Назва].ToString()
+					));
+
+				if (DirectoryPointerItem != null && selectRow == 0) //??
+					if (cur.UnigueID.ToString() == DirectoryPointerItem.UnigueID.ToString())
+					{
+						dataGridViewRecords.Rows[0].Selected = false;
+						dataGridViewRecords.Rows[RecordsBindingList.Count - 1].Selected = true;
+					}
+			}
+
+			if (selectRow != 0 && selectRow < dataGridViewRecords.Rows.Count)
+			{
+				dataGridViewRecords.Rows[0].Selected = false;
+				dataGridViewRecords.Rows[selectRow].Selected = true;
+			}
+		}
+
+		private class Записи
+		{
+			public Записи(string _id, string _Назва)
+			{
+				ID = _id;
+				Назва = _Назва;
+			}
+			public string ID { get; set; }
+			public string Назва { get; set; }
+		}
+
+        private void dataGridViewRecords_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+			if (e.RowIndex >= 0 && e.RowIndex < dataGridViewRecords.RowCount)
+			{
+				string Uid = dataGridViewRecords.Rows[e.RowIndex].Cells["ID"].Value.ToString();
+
+				if (DirectoryControlItem != null)
+				{
+					DirectoryControlItem.DirectoryPointerItem = new Довідники.Організації_Pointer(new UnigueID(Uid));
+					this.Close();
+				}
+				else
+				{
+					toolStripButtonEdit_Click(this, null);
+				}
+			}
+		}
+
+        private void toolStripButtonAdd_Click(object sender, EventArgs e)
+        {
+			Form_ОрганізаціїЕлемент form_ОрганізаціїЕлемент = new Form_ОрганізаціїЕлемент();
+			form_ОрганізаціїЕлемент.IsNew = true;
+			form_ОрганізаціїЕлемент.OwnerForm = this;
+			form_ОрганізаціїЕлемент.ShowDialog();
+        }
+
+        private void toolStripButtonEdit_Click(object sender, EventArgs e)
+        {
+			if (dataGridViewRecords.SelectedRows.Count > 0)
+			{
+				int RowIndex = dataGridViewRecords.SelectedRows[0].Index;
+
+				Form_ОрганізаціїЕлемент form_ОрганізаціїЕлемент = new Form_ОрганізаціїЕлемент();
+				form_ОрганізаціїЕлемент.IsNew = false;
+				form_ОрганізаціїЕлемент.OwnerForm = this;
+				form_ОрганізаціїЕлемент.Uid = dataGridViewRecords.Rows[RowIndex].Cells[0].Value.ToString();
+				form_ОрганізаціїЕлемент.ShowDialog();
+			}			
+		}
+
+        private void toolStripButtonRefresh_Click(object sender, EventArgs e)
+        {
+			LoadRecords();
+		}
+
+        private void toolStripButtonCopy_Click(object sender, EventArgs e)
+        {
+			if (dataGridViewRecords.SelectedRows.Count != 0 &&
+				MessageBox.Show("Копіювати записи?", "Повідомлення", MessageBoxButtons.YesNo) == DialogResult.Yes)
+			{
+				for (int i = 0; i < dataGridViewRecords.SelectedRows.Count; i++)
+				{
+					DataGridViewRow row = dataGridViewRecords.SelectedRows[i];
+					string uid = row.Cells[0].Value.ToString();
+
+                    Довідники.Організації_Objest організації_Objest = new Довідники.Організації_Objest();
+                    if (організації_Objest.Read(new UnigueID(uid)))
+                    {
+						Довідники.Організації_Objest організації_Objest_Новий = організації_Objest.Copy();
+						організації_Objest_Новий.Назва = "Копія - " + організації_Objest_Новий.Назва;
+						організації_Objest_Новий.Save();
+					}
+                    else
+                    {
+                        MessageBox.Show("Error read");
+                        break;
+                    }
+                }
+
+				LoadRecords();
+			}
+		}
+
+        private void toolStripButtonDelete_Click(object sender, EventArgs e)
+        {
+			if (dataGridViewRecords.SelectedRows.Count != 0 &&
+				MessageBox.Show("Видалити записи?", "Повідомлення", MessageBoxButtons.YesNo) == DialogResult.Yes)
+			{
+				for (int i = 0; i < dataGridViewRecords.SelectedRows.Count; i++)
+				{
+					DataGridViewRow row = dataGridViewRecords.SelectedRows[i];
+					string uid = row.Cells[0].Value.ToString();
+
+                    Довідники.Організації_Objest організації_Objest = new Довідники.Організації_Objest();
+                    if (організації_Objest.Read(new UnigueID(uid)))
+                    {
+						організації_Objest.Delete();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Error read");
+                        break;
+                    }
+                }
+
+				LoadRecords();
+			}
+		}
+    }
+}
