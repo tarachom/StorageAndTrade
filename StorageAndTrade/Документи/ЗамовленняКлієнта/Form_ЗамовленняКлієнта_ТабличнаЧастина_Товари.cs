ac751d08f332ec25360925aaf4580fd19b25f1e7
tarachom <tarachom@i.ua>
2022-08-12 17:46:33 +0300

    up

--- StorageAndTrade/Документи/ЗамовленняКлієнта/Form_ЗамовленняКлієнта_ТабличнаЧастина_Товари.cs
+++ StorageAndTrade/Документи/ЗамовленняКлієнта/Form_ЗамовленняКлієнта_ТабличнаЧастина_Товари.cs
@@ -1,4 +1,24 @@
-﻿using System;
+﻿/*
+Copyright (C) 2019-2020 TARAKHOMYN YURIY IVANOVYCH
+All rights reserved.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+/*
+Автор:    Тарахомин Юрій Іванович
+Адреса:   Україна, м. Львів
+Сайт:     accounting.org.ua
+*/
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -109,7 +129,7 @@ namespace StorageAndTrade
 			querySelect.FieldAndAlias.Add(
 				new NameValue<string>(Довідники.ВидиЦін_Const.TABLE + "." + Довідники.ВидиЦін_Const.Назва, "vidy_cen"));
 			querySelect.Joins.Add(
-				new Join(Довідники.ВидиЦін_Const.TABLE, Документи.РеалізаціяТоварівТаПослуг_Товари_TablePart.ВидЦіни, querySelect.Table));
+				new Join(Довідники.ВидиЦін_Const.TABLE, Документи.ЗамовленняКлієнта_Товари_TablePart.ВидЦіни, querySelect.Table));
 
 			//ORDER
 			querySelect.Order.Add(Документи.ЗамовленняКлієнта_Товари_TablePart.НомерРядка, SelectOrder.ASC);
--- StorageAndTrade/Документи/ПоверненняТоварівВідКлієнта/Form_ПоверненняТоварівВідКлієнта_ТабличнаЧастина_Товари.cs
+++ StorageAndTrade/Документи/ПоверненняТоварівВідКлієнта/Form_ПоверненняТоварівВідКлієнта_ТабличнаЧастина_Товари.cs
@@ -1,4 +1,24 @@
-﻿using System;
+﻿/*
+Copyright (C) 2019-2020 TARAKHOMYN YURIY IVANOVYCH
+All rights reserved.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+/*
+Автор:    Тарахомин Юрій Іванович
+Адреса:   Україна, м. Львів
+Сайт:     accounting.org.ua
+*/
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -101,7 +121,7 @@ namespace StorageAndTrade
 			querySelect.FieldAndAlias.Add(
 				new NameValue<string>(Довідники.СеріїНоменклатури_Const.TABLE + "." + Довідники.СеріїНоменклатури_Const.Номер, "seria_number"));
 			querySelect.Joins.Add(
-				new Join(Довідники.СеріїНоменклатури_Const.TABLE, Документи.РеалізаціяТоварівТаПослуг_Товари_TablePart.Серія, querySelect.Table));
+				new Join(Довідники.СеріїНоменклатури_Const.TABLE, Документи.ПоверненняТоварівВідКлієнта_Товари_TablePart.Серія, querySelect.Table));
 
 			//ORDER
 			querySelect.Order.Add(Документи.ПоверненняТоварівВідКлієнта_Товари_TablePart.НомерРядка, SelectOrder.ASC);
